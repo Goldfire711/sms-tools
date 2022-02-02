ab1b48412cf6f73f11f37d706c2656afa3423c2f
Goldfire <goldfire.711@gmail.com>
2022-02-03 02:04:23 +0900

    ObjectParameters (wip)

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -5,9 +5,7 @@ Test::Test(QWidget* parent)
   : QWidget(parent) {
   ui.setupUi(this);
 
-  QStringList lists = { "1", "2", "3", "4", "5" };
-  if (lists.contains("1"))
-    ui.label->setText("true");
+  
   
 }
 
