a3f1dec5605fa985dcfa5e33c00b7c8128dc87af
Goldfire <goldfire.711@gmail.com>
2022-06-23 03:11:16 +0900

    ChuuHanaViewer: Add ChuuHana and Mirror's rotation angle チュウハナの表示と鏡Lの回転角の表示を追加

--- SMS/ChuuHana/ChuuHanaViewer.cpp
+++ SMS/ChuuHana/ChuuHanaViewer.cpp
@@ -58,4 +58,43 @@ void ChuuHanaViewer::paintEvent(QPaintEvent* event) {
   painter.drawPolygon(polygon, 20);
   painter.drawLine(polygon[0], polygon[10]);
   painter.drawLine(polygon[5], polygon[15]);
+
+  // 回転角度を求める
+  // 行列のy軸成分を(0,1,0)に戻す回転行列をx軸成分に掛ける
+  double yx = mtx34[1], yy = mtx34[5], yz = mtx34[9];
+  double xx = mtx34[0], xy = mtx34[4], xz = mtx34[8];
+  double cosine = (yy + yz * yz / (1 + yy)) * xx + -yx * xy - yx * yz / (1 + yy) * xz;
+  double sine = -yx * yz / (1 + yy) * xx - yz * xy + (yy + yx * yx / (1 + yy)) * xz;
+  double theta = acos(cosine);
+  if (sine < 0)
+    theta *= -1;
+  double theta_degree = theta / (2 * M_PI) * 360;
+  ui.label_degree->setText(QString::number(theta_degree));
+
+  // チュウハナのレール
+  QPoint chuuhana_rail[7] = {
+    QPoint(-3300, -7000),
+    QPoint(-3200, -6400),
+    QPoint(-2800, -6100),
+    QPoint(-2122, -6106),
+    QPoint(-1700, -6800),
+    QPoint(-2099, -7661),
+    QPoint(-2900, -7600)
+  };
+  painter.drawPolygon(chuuhana_rail, 7);
+
+  // チュウハナ
+  u32 p_chuuhana_manager = read_u32(0x81097c40 + 0x18);
+  u32 p_chuuhana[3];
+  for (u32 i = 0; i < 3; i++) {
+    p_chuuhana[i] = read_u32(p_chuuhana_manager + (i + 3) * 4);
+  }
+  QPointF chuuhana_points[3];
+  for (u32 i = 0; i < 3; i++) {
+    float x = read_float(p_chuuhana[i] + 0x10);
+    float z = read_float(p_chuuhana[i] + 0x18);
+    chuuhana_points[i] = QPointF(x, z);
+    painter.drawEllipse(x - 225, z - 225, 450, 450);
+  }
+
 }
